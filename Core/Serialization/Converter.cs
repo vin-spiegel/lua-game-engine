6fe3cb5d2392722e5a7c002bf6761e8a03f2d5b6
Spiegel <seoksuho@gmail.com>
2022-10-23 00:46:38 +0900

    refactor Json Serializer

---
+++ Core/Serialization/Converter.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace GameEngineDemo2.Core.Serialization;
+
+public static class Converter
+{
+    public static T? Deserialize<T>(string filename)
+    {
+        return JsonConvert.DeserializeObject<T>(File.ReadAllText(filename), new JsonSerializerSettings()
+        {
+            Formatting = Formatting.Indented
+        });
+    }
+    
+    public static string Serialize(object obj)
+    {
+        return JsonConvert.SerializeObject(obj, Formatting.Indented);
+    }
+}
